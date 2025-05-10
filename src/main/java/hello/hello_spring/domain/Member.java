9b753a77d99c5745841bb2e5056bb0a706c05b32
박소영 <pegasus@ewha.ac.kr>
2025-05-10 21:09:50 +0900

    4~5chapter

---
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hello_spring.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long l) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String spring) {
+        this.name = name;
+    }
+}
